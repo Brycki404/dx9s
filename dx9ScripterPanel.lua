41bac390890475d7844b8381886b90ebcf15cf2f
B0NBunny <dustinbk@icloud.com>
2025-08-16 23:05:56 -0400

    Making new script

---
+++ dx9ScripterPanel.lua
@@ -0,0 +1,202 @@
+--indent size 4
+local startTime = os.clock()
+
+config = _G.config or {
+	urls = {
+		DXLibUI = "https://raw.githubusercontent.com/B0NBunny/DXLibUI/refs/heads/main/main.lua";
+        LibESP = "https://raw.githubusercontent.com/B0NBunny/DXLibESP/refs/heads/main/main.lua";
+	};
+	settings = {
+		menu_toggle_keybind = "[F2]";
+
+		fps = 1; -- 1 = 60, 2 = 120, 3 = 144, 4 = 240
+
+		maximum_Hz_Cache = 15;
+		Sec_precision = 4;
+		Hz_precision = 0;
+	};
+}
+if _G.config == nil then
+	_G.config = config
+	config = _G.config
+end
+
+if _G.averageHz == nil then
+	_G.averageHz = 0
+end
+
+if _G.averageSec == nil then
+	_G.averageSec = 0
+end
+
+if _G.clearedConsole == nil then
+	_G.clearedConsole = false
+end
+
+if _G.consoleEnabled == nil then
+	_G.consoleEnabled = false
+end
+
+if _G.clearedConsole == true then
+	_G.clearedConsole = false
+end
+
+if _G.lastElapsedCycleTimesCache == nil then
+	_G.lastElapsedCycleTimesCache = {}
+elseif _G.lastElapsedCycleTimesCache ~= nil then
+	local cache_entries = #_G.lastElapsedCycleTimesCache
+	if cache_entries >= 1 then
+		local sum = 0
+		for index, elapsedCycleTime in ipairs(_G.lastElapsedCycleTimesCache) do
+			sum = sum + elapsedCycleTime
+		end
+		local averageSeconds = sum / cache_entries
+		local Sec_precision = 10 ^ config.settings.Sec_precision
+		local flooredSec = math.floor(averageSeconds * Sec_precision) / Sec_precision
+		_G.averageSec = flooredSec or 0
+		if averageSeconds > 0 and averageSeconds < math.huge then
+			local averageHertz = 1 / averageSeconds
+			local Hz_precision = 10 ^ config.settings.Hz_precision
+			local flooredHertz = math.floor(averageHertz * Hz_precision) / Hz_precision
+			_G.averageHz = flooredHertz or 0
+		else
+			_G.averageHz = 0
+		end
+	end
+end
+
+lib_ui = loadstring(dx9.Get(config.urls.DXLibUI))()
+
+lib_esp = loadstring(dx9.Get(config.urls.LibESP))()
+
+interface = lib_ui:CreateWindow({
+	Title = "Scripter Panel | dx9ware | By @Brycki";
+	Size = { 500, 500 };
+	Resizable = true;
+
+	ToggleKey = config.settings.menu_toggle_keybind;
+
+	FooterToggle = true;
+	FooterRGB = true;
+	FontColor = { 255, 255, 255 };
+	MainColor = { 25, 25, 25 };
+	BackgroundColor = { 20, 20, 20 };
+	AccentColor = { 255, 50, 255 };
+	OutlineColor = { 40, 40, 40 };
+})
+
+tabs = {}
+tabs.game = interface:AddTab("Game")
+tabs.debugging = interface:AddTab("Debugging")
+tabs.tools = interface:AddTab("Tools")
+
+groupboxes = {}
+groupboxes.game_settings = tabs.game:AddMiddleGroupbox("Game Settings")
+groupboxes.debugging = tabs.debugging:AddMiddleGroupbox("Debugging")
+
+debugging = {}
+debugging.console = groupboxes.debugging:AddToggle({
+		Default = false;
+		Text = "Console Enabled";
+	}):OnChanged(function(value)
+		lib_ui:Notify(value and "[debugging] Enabled Console" or "[debugging] Disabled Console", 1)
+		if value then
+			_G.consoleEnabled = true
+			dx9.ClearConsole()
+			dx9.ShowConsole(true)
+		else
+			_G.consoleEnabled = false
+			dx9.ClearConsole()
+			dx9.ShowConsole(false)
+		end
+	end)
+debugging.sec = groupboxes.debugging:AddLabel("Avg. Program Cycle: ".._G.averageSec.." s")
+debugging.hz = groupboxes.debugging:AddLabel("Avg. Program Cycle: ".._G.averageHz.." Hz")
+debugging.clock = groupboxes.debugging:AddLabel("clock: "..os.clock())
+
+game_settings = {}
+game_settings.fps = groupboxes.game_settings:AddDropdown({
+		Text = "Your Game's FPS";
+		Default = config.settings.fps;
+		Values = { "60", "120", "144", "240" };
+	})
+	:OnChanged(function(value)
+		lib_ui:Notify("[settings] FPS: " .. value, 1)
+	end)
+
+if _G.Get_Distance == nil then
+	_G.Get_Distance = function(v1, v2)
+		local a = (v1.x - v2.x)
+		local b = (v1.y - v2.y)
+		local c = (v1.z - v2.z)
+
+		return math.sqrt(a^2 + b^2 + c^2)
+	end
+end
+
+if _G.Get_Index == nil then
+	_G.Get_Index = function(type, value)
+		local table = nil
+		if type == "game" then
+			table = { "Blade Ball", "Death Ball" }
+		end
+
+		if table then
+			for index, item in pairs(table) do
+				if item == value then
+					return index
+				end
+			end
+		end
+
+		return nil
+	end
+end
+
+datamodel = dx9.GetDatamodel()
+workspace = dx9.FindFirstChild(datamodel, "Workspace")
+services = {
+	players = dx9.FindFirstChild(datamodel, "Players");
+}
+
+local_player = nil
+local_player_table = dx9.get_localplayer()
+current_fps = game_settings.fps and game_settings.fps.Value and tonumber(game_settings.fps.Value) or 60
+
+if local_player == nil then
+	for _, player in pairs(dx9.GetChildren(services.players)) do
+		local pgui = dx9.FindFirstChild(player, "PlayerGui")
+		if pgui ~= nil and pgui ~= 0 then
+			local_player = player
+			break
+		end
+	end
+end
+
+function get_local_player_name()
+	if local_player ~= nil and local_player ~= 0 and dx9.GetType(local_player) == "Player" then
+		return dx9.GetName(local_player)
+	else
+		local_player_table = dx9.get_localplayer()
+		return local_player_table.Info.Name
+	end
+end
+
+local_player_name = get_local_player_name()
+
+my_player = dx9.FindFirstChild(services.players, local_player_name)
+my_character = nil
+my_root = nil
+
+if my_player == nil and my_player == 0 then
+    return
+end
+
+local endTime = os.clock()
+local elapsedTime = endTime - startTime
+if _G.lastElapsedCycleTimesCache ~= nil then
+	if #_G.lastElapsedCycleTimesCache >= config.settings.maximum_Hz_Cache then
+		table.remove(_G.lastElapsedCycleTimesCache, 1)
+	end
+	table.insert(_G.lastElapsedCycleTimesCache, elapsedTime)
+end
